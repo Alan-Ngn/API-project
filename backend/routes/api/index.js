8a8d3ea809ee3634309b1f1b2de18d335295747b
Alan Nguyen <alanngn21@gmail.com>
2023-03-16 14:11:14 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -6,54 +8,12 @@ const { restoreUser } = require("../../utils/auth.js");
   // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
-// stuff after testing phase 3?
-// const router = require('express').Router();
+router.use('/session', sessionRouter);
 
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
+router.use('/users', usersRouter);
 
-// const { restoreUser } = require('../../utils/auth.js');
-// router.use(restoreUser);
-
-// const { requireAuth } = require('../../utils/auth.js');
-
-
-// GET /api/restore-user
-//Make sure to keep the restoreUser middleware connected before any other middleware or route handlers are connected to the router.
-// router.get(
-//   '/restore-user',
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// Once you are satisfied with the test results, you can remove all code for testing the user auth middleware routes.
-// GET /api/set-token-cookie
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'Demo-lition'
-//     }
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user: user });
-// });
-
-
-// // GET /api/require-auth
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//   });
-
-
-
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
