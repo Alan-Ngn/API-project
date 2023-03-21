f570fbbd0508f9e4b0ea1db01124885ed936f357
Alan Nguyen <alanngn21@gmail.com>
2023-03-21 13:12:25 -0700

    booking seed complete

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -9,11 +9,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'ownerId',
       })
 
-      // User.belongsToMany(models.Spot, {
-      //   through: models.Booking,
-      //   foreignKey: 'userId',
-      //   otherKey: 'spotId'
-      // })
+      User.belongsToMany(models.Spot, {
+        through: models.Booking,
+        foreignKey: 'userId',
+        otherKey: 'spotId'
+      })
 
       // User.belongsToMany(models.Spot, {
       //   through: models.Review,
