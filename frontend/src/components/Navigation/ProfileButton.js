6c19a2a5ed3531b2e987c4b02f8ed77c4c0a3c6a
Alan Nguyen <alanngn21@gmail.com>
2023-04-07 13:47:36 -0700

    navigation component complete

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,10 @@
+function ProfileButton (){
+    return (
+        <div style={{ color: "blue", fontSize: "100px" }}>
+          <i class="fa-solid fa-user"></i>
+        </div>
+      );
+    };
+
+
+export default ProfileButton
